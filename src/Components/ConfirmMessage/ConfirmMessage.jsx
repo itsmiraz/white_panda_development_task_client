3893a5483e52be939d588b9743f0aaf37b51468e
itsmiraz <mirajulislamofficial044@gmail.com>
2023-01-13 22:40:58 +0600

    most of the done

--- src/Components/ConfirmMessage/ConfirmMessage.jsx
+++ src/Components/ConfirmMessage/ConfirmMessage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ConfirmMessage = () => {
+const ConfirmMessage = ({ bookedCar, openConfirmModal }) => {
     return (
-        <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXlwndYQDmWUL-65h2Rblh2HAy6nBweQksFw&usqp=CAU" alt="" />
-            <h1></h1>
+        <div className='absolute top-1/4 right-1/3 z-50'>
+            <div className='flex relative p-5 rounded-md bg-white gap-x-4 shadow-2xl  items-center font-semibold'>
+                <img className='w-24' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXlwndYQDmWUL-65h2Rblh2HAy6nBweQksFw&usqp=CAU" alt="" />
+                <div className='pb-14 pt-5 space-y-2 px-4'>
+                    <h1 className='text-2xl '>Booking Confirmed!</h1>
+                    <p>You Have Booked: {bookedCar.title}</p>
+                    <p>for the Duration: {openConfirmModal.issueDateFormat}-{openConfirmModal.ReturnDateFormat}</p>
+                </div>
+                <button className='absolute bottom-2 bg-gray-500  text-white px-4 py-2 right-6 my-2'>
+
+                    <Link to='/'>
+                    Continue
+                    </Link>
+                </button>
+            </div>
+           
         </div>
     );
 };
--- src/Pages/BookACar/BookACar.jsx
+++ src/Pages/BookACar/BookACar.jsx
@@ -9,81 +9,97 @@ import ConfirmMessage from '../../Components/ConfirmMessage/ConfirmMessage';
 
 const BookACar = () => {
     const data = useLoaderData()
-
     const [issueDate, setissueDate] = useState(null)
     const [ReturnDate, setReturnDate] = useState(null)
-
-const [addBook] = useAddBookMutation()
+    const [openConfirmModal, setOpenConfirmModal] = useState('')
+    const [addBook] = useAddBookMutation()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const issueDateFormat = format(issueDate, 'PP')
         const ReturnDateFormat = format(ReturnDate, 'PP')
         if (issueDateFormat >= ReturnDateFormat) {
-           alert('Please Add A validable retun date')
+            alert('Please Add A validable retun date')
         }
         else {
             const form = e.target;
             const bookingDetails = {
-                carId:data._id,
+                carId: data._id,
                 name: form.name.value,
                 phone: form.contact.value,
                 issueDateFormat,
                 ReturnDateFormat
             }
             console.log(bookingDetails)
-            addBook(bookingDetails)
+            // addBook(bookingDetails)
+            setOpenConfirmModal(bookingDetails)
         }
 
-       
+
 
     }
 
     return (
-        <section className='p-20'>
-            <h1 className='font-semibold text-xl'>Booking Details</h1>
-            <form action="" className=' w-[600px]' onSubmit={handleSubmit}>
+        <section className='p-20 grid grid-cols-2 justify-items-center place-items-center'>
+            <div>
+                <img src={ data.img} alt="" />
+            </div>
+            <div>
+                <h1 className='font-semibold text-xl'>Booking Details</h1>
+                <form action="" className=' w-[600px]' onSubmit={handleSubmit}>
 
-                <div className='grid gap-10 font-semibold  grid-cols-2 justify-between items-center w-[600px] my-5'>
-                    <div>
-                        <p className='font-semibold text-xl'>Name</p>
-                        <input required name='name' type="text" placeholder='Your Name' className='p-2 border-b-2' />
+                    <div className='grid gap-10 font-semibold  grid-cols-2 justify-between items-center w-[600px] my-5'>
+                        <div>
+                            <p className='font-semibold text-xl'>Name</p>
+                            <input required name='name' type="text" placeholder='Your Name' className='p-2 border-b-2' />
+                        </div>
+                        <div>
+                            <p className='font-semibold text-xl'>Contact Number</p>
+                            <input name='contact' required type="text" placeholder='+91' className='p-2 border-b-2' />
+                        </div>
+                        <div>
+                            <p className='font-semibold text-xl'>Issue Date</p>
+                            <ReactDatePicker
+                                selected={issueDate}
+                                onChange={date => setissueDate(date)}
+                                className='rounded p-2 border-b-2 '
+                                dateFormat='dd/MM/yyy'
+                                minDate={new Date()}
+                                required
+                                placeholderText='DD/MM/YYY'
+                            ></ReactDatePicker>
+                        </div>
+                        <div>
+                            <p className='font-semibold text-xl'>Return Date</p>
+                            <ReactDatePicker
+                                selected={ReturnDate}
+                                onChange={date => setReturnDate(date)}
+                                className='rounded p-2 border-b-2 '
+                                dateFormat='dd/MM/yyy'
+                                minDate={new Date()}
+                                required
+                                placeholderText='DD/MM/YYY'
+                            ></ReactDatePicker>
+                        </div>
                     </div>
-                    <div>
-                        <p className='font-semibold text-xl'>Contact Number</p>
-                        <input name='contact' required type="text" placeholder='+91' className='p-2 border-b-2' />
+                    <div className='flex pr-10 justify-between'>
+                        <Link to={`/cardetail/${data._id}`} className='px-4 py-2 border border-gray-500 '>Back</Link>
+                        <button type='submit' className='px-4 py-2 font-semibold text-white bg-gray-500'>Submit</button>
                     </div>
-                    <div>
-                        <p className='font-semibold text-xl'>Issue Date</p>
-                        <ReactDatePicker
-                            selected={issueDate}
-                            onChange={date => setissueDate(date)}
-                            className='rounded p-2 border-b-2 '
-                            dateFormat='dd/MM/yyy'
-                            minDate={new Date()}
-                            required
-                            placeholderText='DD/MM/YYY'
-                        ></ReactDatePicker>
+                </form>
+            </div>
+            {
+                openConfirmModal &&
+                <>
+                    <ConfirmMessage
+                        bookedCar={data}
+                        openConfirmModal={openConfirmModal}
+                    ></ConfirmMessage>
+                    <div className='w-full h-screen bg-black/50 z-10 absolute top-0'>
+
                     </div>
-                    <div>
-                        <p className='font-semibold text-xl'>Return Date</p>
-                        <ReactDatePicker
-                            selected={ReturnDate}
-                            onChange={date => setReturnDate(date)}
-                            className='rounded p-2 border-b-2 '
-                            dateFormat='dd/MM/yyy'
-                            minDate={new Date()}
-                            required
-                            placeholderText='DD/MM/YYY'
-                        ></ReactDatePicker>
-                    </div>
-                </div>
-                <div className='flex pr-10 justify-between'>
-                <Link to={`/cardetail/${data._id}`} className='px-4 py-2 border border-gray-500 '>Back</Link>
-                <button type='submit' className='px-4 py-2 font-semibold text-white bg-gray-500'>Submit</button>
-               </div>
-            </form>
-           <ConfirmMessage></ConfirmMessage>
+                </>
+            }
         </section>
     );
 };
